c09b5dcfb836e155bc29147a56cd27ab822a6c58
Éricles Oliveira <ericles.motta01@etec.sp.gov.br>
2019-08-27 08:46:54 -0300

    Criando operacao de instanciacao

--- src/main/java/com/ericles/cursomc/CursomcApplication.java
+++ src/main/java/com/ericles/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.ericles.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ericles.cursomc.domain.Categoria;
+import com.ericles.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Instanciando Objetos para serem salvos no momento que a aplicação inicar
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); //Salvando as categorias como um arraylist de Categoria
+		
+
 	}
 
 }
