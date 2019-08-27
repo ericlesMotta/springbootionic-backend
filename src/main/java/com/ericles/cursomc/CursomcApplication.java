bf9295f62d478aeb706b0a14ceb1db5e4035f83f
Éricles Oliveira <ericles.motta01@etec.sp.gov.br>
2019-08-27 13:43:06 -0300

    Cliente,TipoCliente,telefones e enderecos

--- src/main/java/com/ericles/cursomc/CursomcApplication.java
+++ src/main/java/com/ericles/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ericles.cursomc.domain.Categoria;
 import com.ericles.cursomc.domain.Cidade;
+import com.ericles.cursomc.domain.Cliente;
+import com.ericles.cursomc.domain.Endereco;
 import com.ericles.cursomc.domain.Estado;
 import com.ericles.cursomc.domain.Produto;
+import com.ericles.cursomc.domain.enums.TipoCliente;
 import com.ericles.cursomc.repositories.CategoriaRepository;
 import com.ericles.cursomc.repositories.CidadeRepository;
+import com.ericles.cursomc.repositories.ClienteRepository;
+import com.ericles.cursomc.repositories.EnderecoRepository;
 import com.ericles.cursomc.repositories.EstadoRepository;
 import com.ericles.cursomc.repositories.ProdutoRepository;
 
@@ -28,6 +33,10 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,42 +44,47 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//Instanciando Objetos para serem salvos no momento que a aplicação inicar
+		// Instanciando Objetos para serem salvos no momento que a aplicação inicar
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto prod1 = new Produto(null, "Computador", 2000.00);
 		Produto prod2 = new Produto(null, "Impressora", 800.00);
 		Produto prod3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(prod1,prod2,prod3));
+
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
 		cat2.getProdutos().addAll(Arrays.asList(prod2));
-		
+
 		prod1.getCategorias().addAll(Arrays.asList(cat1));
-		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		prod3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); //Salvando as categorias como um arraylist de Categoria
-		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
-		
-		
-		Estado est1 = new Estado(null,"Minas Gerais");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); // Salvando as categorias como um arraylist de Categoria
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlandia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-	
 
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "18055133", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "18055143", cli1, c2);
 		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
-		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
 	}
 
--- src/main/java/com/ericles/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/ericles/cursomc/repositories/ClienteRepository.java
@@ -4,9 +4,9 @@ package com.ericles.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ericles.cursomc.domain.Estado;
+import com.ericles.cursomc.domain.Cliente;
 
 @Repository
-public interface EstadoRepository extends JpaRepository<Estado,Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
 
 }
