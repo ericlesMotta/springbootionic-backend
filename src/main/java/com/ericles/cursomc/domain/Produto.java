6a2f4580bf266ccabc658fdf6e49de66dce96348
Éricles Oliveira <ericles.motta01@etec.sp.gov.br>
2019-08-27 09:27:38 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/ericles/cursomc/domain/Produto.java
+++ src/main/java/com/ericles/cursomc/domain/Produto.java
@@ -8,22 +8,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
+	
 	
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //Anotações spring para gerar o DB automaticamente
+
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(
+				name = "PRODUTO_CATEGORIA",
+				joinColumns = @JoinColumn(name = "produto_id"),
+				inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			   )
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -40,7 +49,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -49,35 +58,49 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	public Categoria() {
+	public Produto() {
 		
 	}
-	
-	public Categoria (Integer id, String nome) {
+
+	public Produto(Integer id, String nome, Double preco) {
 		super();
-		this.id = id;
-		this.nome = nome;
+		this.setId(id);
+		this.setNome(nome);
+		this.setPreco(preco);
 	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+	
+	
+	
 }
