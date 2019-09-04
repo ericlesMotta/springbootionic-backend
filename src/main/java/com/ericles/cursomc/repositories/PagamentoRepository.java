35bfa440440125562d052b06a2b5f291297a1075
ericlesMotta <ericles.motta01@etec.sp.gov.br>
2019-09-04 10:49:07 -0300

    Pedido,EstadoPagamento e Pagamento

---
+++ src/main/java/com/ericles/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.ericles.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ericles.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento,Integer> {
+
+}
