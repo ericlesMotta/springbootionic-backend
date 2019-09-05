dc72016c0705aa1c52a2e92eba4427ed4dc5efa2
ericlesMotta <ericles.motta01@etec.sp.gov.br>
2019-09-05 16:16:24 -0300

    Listando Todas Categorias com DTO

--- src/main/java/com/ericles/cursomc/services/CategoriaService.java
+++ src/main/java/com/ericles/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ericles.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException ex) {
 			throw new DataIntegretyException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
